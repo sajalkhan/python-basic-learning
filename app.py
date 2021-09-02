0cb67aef8548b118cdebafcb81143dcf20d9fa16
sajalkhan <sajalhossen@yahoo.com>
2021-09-02 12:01:31 +0600

    different type of python comments

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+# this is a single line comments
 print('hello world!')
+
+print('wow python is awesome!')  # wow this is awesome is single line comments
+
+"""
+wow this is a multi line commends
+it's some time's really helpfull
+i really like it so much 
+"""
+print('it\'s a multi line comments')
